5e3a4c690a419857a8ca510ec5d524270a92163f
moni0126 <59993927+moni0126@users.noreply.github.com>
2020-01-29 20:33:08 +0900

    Add files via upload

---
+++ if 文.java
@@ -0,0 +1,35 @@
+package homework;
+
+public class Homework {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+
+
+		int a = 5 ;
+
+		if (a == 1)
+		{
+			System.out.println("aの値が1" );
+
+		} else if(a == 2)
+		{
+			System.out.println("aの値が2" );
+
+		} else if (a == 3)
+		{
+			System.out.println("aの値が3" );
+
+		}else if (a == 4)
+		{
+			System.out.println("aの値が4" );
+
+		}else
+		{
+			System.out.println("aの値＝" + a);
+		}
+
+		
+	}
+
+}
